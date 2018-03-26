07239edc0eea00cf5295b72e8d2a51ec6b18eac7
mishja <37374238+mishja@users.noreply.github.com>
2018-03-26 17:18:35 +0200

    Game over gefixt babyyy

--- app/src/main/java/com/timothypuglia/scumbagpug/gameoverscreen.java
+++ app/src/main/java/com/timothypuglia/scumbagpug/gameoverscreen.java
@@ -1,9 +1,14 @@
 package com.timothypuglia.scumbagpug;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
